7b549ec4808a160d9d349e30d5e7573acb028337
huhao <925579748@qq.com>
2018-08-29 23:05:37 +0800

    update

--- ldevmanager/routetable.c
+++ ldevmanager/routetable.c
@@ -3,24 +3,28 @@
 // 当前模块不具备多线程能力
 #include <stdio.h>
 #include <time.h>
+#include <semaphore.h> // Posix标准信号量 pthread库
 #include "routetable.h"
 
-#define ID_BYTE_BEGING 0x01 // ID池起始位
-#define ID_BYTE_END 0xFF    // ID池结束
+#define ID_BYTE_BEGING 0x01 // ID池起始位置
+#define ID_BYTE_END 0xFF    // ID池结束位置
 
 #define IDPOOL_SIZE 2048 // 最大id分配数 UPDATE: 可通过配置文件修改
-#define ID_FREE '0'     // 当前ID未使用
-#define ID_USED '1'     // 当前ID已使用
-#define ID_OUTTIME '2'  // 当前ID超时状态未知
+#define ID_FREE '0'      // 当前ID未使用
+#define ID_USED '1'      // 当前ID已使用
+#define ID_OUTTIME '2'   // 当前ID超时状态未知
 
 // 路由表和ID池本地库路径
 #define ID_POOLFILE_PATH "./idpool.db"      // ID池管理文件
 #define ID_ROUTEFILE_PATH "./routetable.db" // 路由表路径
 
-static char ID_POOL[IDPOOL_SIZE + 1];       // ID池
-static int id_cur;                         // ID池游标
-static Routetable Route_table[IDPOOL_SIZE]; // 路由表数据结构
+static char *ID_POOL;           // ID池
+static int id_cur;              // ID池游标
+static Routetable *Route_table; // 路由表数据结构
+static sem_t mutex_id;          // id池互斥锁
+static sem_t mutex_route;       // route互斥锁
 
+// 时间获取函数
 time_t get_time()
 {
     time_t t;
@@ -28,22 +32,35 @@ time_t get_time()
     return t;
 }
 
-// 路由表/id初始化函数
-// 从数据库配置文件
-// @param
-// @return 错误返回 0正常
-int routetable_init()
+// 路由表和ID统一初始化
+void route_id_init(void)
 {
-    // 1.id管理初始化
-    // 2.routetable初始化
+    int ret, size;
+    // 配置id及路由表的大小
+    size = IDPOOL_SIZE;
+    ID_POOL = (char *)malloc(sizeof(char) * (size + 1));
+    Route_table = (Routetable *)malloc(sizeof(Routetable) * (size));
+    // 初始化无名信号量互斥锁
+    ret = sem_init(&mutex_id, 0, 1);    // id信号量
+    ret = sem_init(&mutex_route, 0, 1); // route信号量
+    // 路由表和ID初始化
+    idpool_init();
+    route_init();
+}
+
+// 读取id池文件，将信息读入内存
+// @param
+// @return 返回0
+int idpool_init(void)
+{
+    // id池初始化
     int i, ret;
     FILE *ifp = NULL; // id文件指针
-    FILE *rfp = NULL; // 路由表文件指针
     // ID文件打开读取
     ifp = fopen(ID_POOLFILE_PATH, "r"); // 打开失败 创建文件
     if (ifp == NULL)
     {
-        // LOG: 记录日志
+        // 初始化id池文件
         ifp = fopen(ID_POOLFILE_PATH, "w+");
         for (i = 0; i < IDPOOL_SIZE; i++)
         {
@@ -62,10 +79,108 @@ int routetable_init()
     }
     else
     {
+        // 读取id池文件至内存
         fscanf(ifp, "%d:%s", &id_cur, ID_POOL);
     }
     fclose(ifp);
+    return 0;
+}
 
+// ID分配函数
+// 根据id池分配id，采用互斥锁
+// @param
+// @return 正确返回id号 错误返回-1
+int id_Alloca(void)
+{
+    sem_wait(&mutex_id);
+    int i;
+    for (i = id_cur + 1; i <= IDPOOL_SIZE; i++)
+    {
+        if (i == IDPOOL_SIZE)
+            i = ID_BYTE_BEGING;
+        if (ID_POOL[i] == ID_FREE)
+        {
+            ID_POOL[i] = ID_USED;
+            id_cur = i;
+            break;
+        }
+        if (i == id_cur)
+        {
+            i = -1;
+            break;
+        }
+    }
+    sem_post(&mutex_id);
+    return i;
+}
+
+// ID释放函数
+// @param
+// @id 输入需释放的id
+// @return 正确返回0
+void id_Release(int id)
+{
+    if (id >= ID_BYTE_END || id < ID_BYTE_BEGING)
+        return 0x0301;
+    if ((id & 0x00FF) == 0x000D) // 去除0x0D的影响
+    {
+        ID_POOL[id] = ID_USED;
+    }
+    else
+    {
+        ID_POOL[id] = ID_FREE;
+    }
+    return 0;
+}
+
+// id池文件更新
+// 将内存中id池写入文件当中
+// @param
+// @return 0正常
+int id_Update(void)
+{
+    sem_wait(&mutex_id);
+    int i, ret;
+    FILE *ifp = NULL; // id文件指针
+    // ID文件打开读取
+    ifp = fopen(ID_POOLFILE_PATH, "r"); // 打开失败则创建文件
+    if (ifp == NULL)
+    {
+        // 初始化id池文件
+        ifp = fopen(ID_POOLFILE_PATH, "w+");
+        for (i = 0; i < IDPOOL_SIZE; i++)
+        {
+            if ((i & 0x00FF) == 0x000D) // 去除0x0D的影响
+            {
+                ID_POOL[i] = ID_USED;
+            }
+            else
+            {
+                ID_POOL[i] = ID_FREE;
+            }
+        }
+        ID_POOL[IDPOOL_SIZE] = '\0';
+        fprintf(ifp, "%d:%s", ID_BYTE_BEGING, ID_POOL);
+        id_cur = ID_BYTE_BEGING;
+    }
+    else
+    {
+        // 内存中id池数据写入文件
+        fprintf(ifp, "%d:%s", ID_BYTE_BEGING, ID_POOL);
+    }
+    fclose(ifp);
+    sem_post(&mutex_id);
+    return 0;
+}
+
+// 路由表初始化函数
+// @param
+// @return 错误返回 0正常
+int route_init(void)
+{
+    // routetable初始化
+    int ret;
+    FILE *rfp = NULL; // 路由表文件指针
     // ROUTETABLE文件打开读取
     rfp = fopen(ID_ROUTEFILE_PATH, "r");
     if (rfp == NULL)
@@ -90,94 +205,64 @@ int routetable_init()
     return 0;
 }
 
-// ID分配
-// @param
-// @return 正确返回id号 错误返回-1
-int id_alloc()
-{
-    int i;
-    for (i = id_cur + 1; i <= ID_NUM_MAX; i++)
-    {
-        if (i == ID_NUM_MAX)
-            i = ID_BYTE_BEGING;
-        if (ID_POOL[i] == ID_FREE)
-        {
-            ID_POOL[i] = ID_USED;
-            id_cur = i;
-            return i;
-        }
-        if (i == id_cur)
-            return -1;
-    }
-}
-
-// ID释放
-void releseid(int id)
-{
-    if (id >= ID_NUM_MAX)
-        return;
-    if ((id & 0x00FF) == 0x000D) // 去除0x0D的影响
-    {
-        ID_POOL[id] = ID_USED;
-    }
-    else
-    {
-        ID_POOL[id] = ID_FREE;
-    }
-}
-
 // 路由表管理
-// 加入表
-int join_route(int id, int com)
+// 将新的设备id加入表
+// @param
+// @id 设备id
+// @dtype 设备类型/接口编号
+// @return 错误返回
+int route_join(int id, int dtype)
 {
-    if (id >= ID_NUM_MAX)
-        return -1;
+    // 判断id是否越界
+    if (id >= IDPOOL_SIZE || id < ID_BYTE_BEGING)
+        return 0x0302;
+
+    sem_wait(&mutex_route);
     // FIXME: 应该使用动态数据结构 为了节省开发时间 采用当前模式
-    Route_table[id].type = com;
+    Route_table[id].id = id;
+    Route_table[id].dtype = dtype;
     Route_table[id].stat = DEV_STA_ACTIVE;
-    // Route_table[id].last_time = get_time();
+    Route_table[id].reg_time = get_time();
+    Route_table[id].last_time = get_time();
+    sem_post(&mutex_route);
+    return 0;
 }
 
-// 匹配表
-int match_route(int id, int *com)
+// 根据id获取，对应id设备/接口编号
+// @param
+// @id 需查询的设备id
+// @dtype 返回对应设备id的接口编号
+// @return 错误返回
+int route_march(int id, int *dtype)
 {
-    if (id >= ID_NUM_MAX)
-        return -1;
-    if (Route_table[id].type == 0)
+    if (id >= IDPOOL_SIZE || id < ID_BYTE_BEGING || dtype == NULL)
+        return 0x0302;
+
+    if (Route_table[id].id == 0)
     {
-        return -1;
+        *dtype = DEV_TYPE_NONE;
+        return 0x0303;
     }
-    if (com == NULL)
-    {
-        return 0;
-    }
-    *com = Route_table[id].type;
+    *dtype = Route_table[id].dtype;
     return 0;
 }
 
 // 删除表
-int delete_route(int id)
+// @param
+// @id 需释放的设备id
+// @return 返回错误
+int route_release(int id)
 {
-    if (id >= ID_NUM_MAX)
-        return -1;
-    Route_table[id].type = 0;
-    Route_table[id].stat = 0;
+    if (id >= IDPOOL_SIZE || id < ID_BYTE_BEGING)
+        return 0x0302;
+    Route_table[id].id = 0;
+    Route_table[id].dtype = DEV_TYPE_NONE;
+    Route_table[id].stat = DEV_STA_NONE;
+    Route_table[id].reg_time = 0;
     Route_table[id].last_time = 0;
+    return 0;
 }
 
-// 修改表
-//
-int modify_route(int id, int sta)
-{
-    if (id >= ID_NUM_MAX)
-        return -1;
-    Route_table[id].stat = sta;
-    Route_table[id].last_time = 0;
-}
-
-// 检查表
-void check_route(void)
-{
-
-    // 检查超时
-}
+int route_modify(int id, int sta, int dtype);
+int route_maintain(void);
+int route_update(void);
--- ldevmanager/routetable.h
+++ ldevmanager/routetable.h
@@ -2,12 +2,14 @@
 #define _ROUTETABLE_H_
 
 // 路由表中设备类型
+#define DEV_TYPE_NONE 0x00
 #define DEV_TYPE_WIFI 0x01
 #define DEV_TYPE_BLUETOOTH 0x02
 #define DEV_TYPE_LORA 0x03
 #define DEV_TYPE_NB 0x04
 
 // 路由表中状态字段
+#define DEV_STA_NONE 0x00
 #define DEV_STA_ACTIVE 0x01
 #define DEV_STA_OUTTIME 0x02
 #define DEV_STA_MAYBEDEAD 0x03
@@ -16,25 +18,27 @@
 typedef struct _Routetable
 {
     int id;
-    int type;
+    int dtype;
     int stat;
     int reg_time;
     int last_time;
 } Routetable;
 
-int route_id_init(void);
+void route_id_init(void);
 
 // ID管理
 int idpool_init(void);
 int id_Alloca(void);
 int id_Release(int id);
+int id_Update(void);
 
 // 路由表管理
 int route_init(void);
 int route_join(int id, int type);
-int route_march(int id, int type);
+int route_march(int id, int *type);
 int route_release(int id);
-int route_modify(int id, int sta);
+int route_modify(int id, int sta, int dtype);
 int route_maintain(void);
+int route_update(void);
 
 #endif
