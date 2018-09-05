8c7619484afccf4a4e3b1fc52bd6e6e124083735
huhao <925579748@qq.com>
2018-09-05 20:56:02 +0800

    update

--- zIndoing/configp/configp.c
+++ zIndoing/configp/configp.c
@@ -1,2 +1,25 @@
 #include "configp.h"
 
+int conf_open(const char *path)
+{
+}
+
+int conf_read(const char *key, char *value)
+{
+}
+
+int conf_update(const char *key, const char *value)
+{
+}
+
+int conf_add(const char *key, const char *value)
+{
+}
+
+int conf_delete(const char *key)
+{
+}
+
+int conf_close(int confd)
+{
+}
--- zIndoing/configp/configp.h
+++ zIndoing/configp/configp.h
@@ -1,6 +1,11 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-
+int conf_open(const char *path);
+int conf_read(const char *key, char *value);
+int conf_update(const char *key, const char *value);
+int conf_add(const char *key, const char *value);
+int conf_delete(const char *key);
+int conf_close(int confd);
 
 #endif
